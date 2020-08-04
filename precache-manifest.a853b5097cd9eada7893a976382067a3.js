6ab9223b8dd3e2402568f0f30d84cdde3be70cd7
Ahmad Ramdani <ahmad.ramdani@imdglobalservices.com>
2020-08-04 15:12:42 +0700

    Updates

---
+++ precache-manifest.a853b5097cd9eada7893a976382067a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f089b856be2932a03673cd92d3a62005",
+    "url": "/xe-web/index.html"
+  },
+  {
+    "revision": "261260d61986b76b2272",
+    "url": "/xe-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35f1f8b992c8db7e4f9b",
+    "url": "/xe-web/static/js/2.9dd798fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/xe-web/static/js/2.9dd798fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261260d61986b76b2272",
+    "url": "/xe-web/static/js/main.802dc6e9.chunk.js"
+  },
+  {
+    "revision": "73b330a981138969547a",
+    "url": "/xe-web/static/js/runtime-main.6d3d9c2e.js"
+  }
+]);
